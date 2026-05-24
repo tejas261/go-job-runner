22d220269d9d3236cf8d2bbe873b51b8caa6fadc
tejasm <tejasm@gofynd.com>
2026-05-24 21:19:07 +0530

    infra setup using terraform

---
+++ infra/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">=1.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "job-runner" {
+  name     = "job-runner-resources"
+  location = "South India"
+}
+
+resource "azurerm_virtual_network" "job-runner" {
+  name                = "job-runner_VNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.job-runner.location
+  resource_group_name = azurerm_resource_group.job-runner.name
+}   
+
+resource "azurerm_subnet" "aks" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.job-runner.name
+  virtual_network_name = azurerm_virtual_network.job-runner.name
+  address_prefixes     = ["10.0.1.0/24"]
+}     
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                  = "job-runner-k8s"
+  location            = azurerm_resource_group.job-runner.location
+  resource_group_name = azurerm_resource_group.job-runner.name
+   dns_prefix          = "job-runner-aks"
+
+    default_node_pool {
+    name       = "agentpool"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+    identity {
+    type = "SystemAssigned"
+  }
+}
